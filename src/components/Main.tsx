e92b026d026dd132e6fb0c757330b69744c91f9e
Allen OYang <allenwill211@gmail.com>
2023-04-25 15:19:12 +0800

    feat: change ui

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,41 +2,42 @@
  * @Author: Allen OYang
  * @Email:  allenwill211@gmail.com
  * @Date: 2023-04-14 12:08:23
- * @LastEditTime: 2023-04-24 22:53:06
+ * @LastEditTime: 2023-04-25 14:19:12
  * @LastEditors: Allen OYang allenwill211@gmail.com
  * @FilePath: /nova-gpt/src/components/Main.tsx
  */
-import { ChatContent } from "@/components/ChatContent";
-import { ChatTextarea } from "@/components/ChatTextareaContainer";
-import { useSettingStore } from "@/stores/SettingStore";
-import { MantineTheme, createStyles } from "@mantine/core";
-import { Setting } from "./Setting";
+import { ChatContent } from '@/components/ChatContent'
+import { ChatTextarea } from '@/components/ChatTextareaContainer'
+import { useSettingStore } from '@/stores/SettingStore'
+import { MantineTheme, createStyles } from '@mantine/core'
+import { Setting } from './Setting'
+import { EmptyChats } from './Empty'
+import { useChatStore } from '@/stores/ChatStore'
 
 const useStyles = createStyles((theme: MantineTheme) => {
   return {
     mainContainer: {
-      display: "flex",
-      flexDirection: "column",
-      height: "100%",
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
     },
     chatTextareaContainer: {},
-  };
-});
+  }
+})
 
 export function Main() {
-  const isSetting = useSettingStore((state) => state.isSetting);
-  const { classes } = useStyles();
+  const isSetting = useSettingStore((state) => state.isSetting)
+  const notKey = useSettingStore((state) => !state.openAI.key)
+  const notChats = useChatStore((state) => !state.chats.length)
+  const { classes } = useStyles()
 
   return (
     <>
-      {isSetting ? (
-        <Setting />
-      ) : (
-        <div className={classes.mainContainer}>
-          <ChatContent />
-          <ChatTextarea />
-        </div>
-      )}
+      <Setting />
+      <div className={classes.mainContainer}>
+        {notKey || notChats ? <EmptyChats /> : <ChatContent />}
+        <ChatTextarea />
+      </div>
     </>
-  );
+  )
 }
--- src/models/ThemeColor.ts
+++ src/models/ThemeColor.ts
@@ -2,14 +2,14 @@
  * @Author: Allen OYang
  * @Email:  allenwill211@gmail.com
  * @Date: 2023-04-23 17:19:37
- * @LastEditTime: 2023-04-24 10:23:09
+ * @LastEditTime: 2023-04-25 14:53:11
  * @LastEditors: Allen OYang allenwill211@gmail.com
- * @FilePath: /speak-gpt/src/models/ThemeColor.ts
+ * @FilePath: /nova-gpt/src/models/ThemeColor.ts
  */
-import { DefaultMantineColor } from "@mantine/core";
+import { DefaultMantineColor } from '@mantine/core'
 export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends Function ? T[P] : DeepPartial<T[P]>;
-};
+  [P in keyof T]?: T[P] extends Function ? T[P] : DeepPartial<T[P]>
+}
 
 // #2d3160
 // #262d43
@@ -27,35 +27,35 @@ export const ThemeColor: DeepPartial<
       string,
       string,
       string,
-      string
+      string,
     ]
   >
 > = {
   // https://smart-swatch.netlify.app/#2d3160
   light: [
-    "#defef0",
-    "#b8f4d9",
-    "#90edc2",
-    "#66e4ab",
-    "#3cdc93",
-    "#23c37a",
-    "#16975e",
-    "#0a6c43",
-    "#014227",
-    "#00180a",
+    '#defef0',
+    '#b8f4d9',
+    '#90edc2',
+    '#66e4ab',
+    '#3cdc93',
+    '#23c37a',
+    '#16975e',
+    '#0a6c43',
+    '#014227',
+    '#00180a',
   ],
   // https://smart-swatch.netlify.app/#2d3160
   dark: [
-    "#efeefb",
-    "#cfcee3",
-    "#aeafce",
-    "#8e90bb",
-    "#6e72a7",
-    "#55598e",
-    "#42476f",
-    "#2f3450",
-    "#1c2031",
-    "#070a15",
+    '#efeefb',
+    '#cfcee3',
+    '#aeafce',
+    '#8e90bb',
+    '#6e72a7',
+    '#55598e',
+    '#42476f',
+    '#2f3450',
+    '#1c2031',
+    '#070a15',
     // "#ededff",
     // "#cbcdea",
     // "#a8acd6",
@@ -68,12 +68,13 @@ export const ThemeColor: DeepPartial<
     // "#060817",
   ],
 
-  darkButton: ["#2a313e"],
+  darkButton: ['#2a313e'],
 
   gradient: [
     `linear-gradient(to bottom left, #202539 30%,  #332538 100%)`,
     `linear-gradient(to right bottom, #8a3ffb 40%, #3646e8)`,
     `linear-gradient(to bottom, #262d42 60%, transparent)`,
     `linear-gradient(to right,  #8a3ffb  20%, #3646e8 60%, #2c3453)`,
+    `linear-gradient(to right,  #8a3ffb  40%, #3646e8 90%, #3646e8)`,
   ],
-};
+}
