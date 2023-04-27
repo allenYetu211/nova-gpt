4527cbb4837972962333b744a33a644f6863493c
Allen OYang <allenwill211@gmail.com>
2023-04-27 19:39:15 +0800

    chore: fix vercel deploy

--- next.config.js
+++ next.config.js
@@ -2,24 +2,13 @@
  * @Author: Allen OYang
  * @Email:  allenwill211@gmail.com
  * @Date: 2023-04-24 14:54:13
- * @LastEditTime: 2023-04-27 15:44:19
+ * @LastEditTime: 2023-04-27 19:38:31
  * @LastEditors: Allen OYang allenwill211@gmail.com
  * @FilePath: /nova-gpt/next.config.js
  */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-
-    return config
-  },
-  output: 'standalone',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
